90647a00d4f72183eee3f42a2f0c5813f5f04b6e
sheey <13375959+Sheey11@users.noreply.github.com>
2021-03-04 23:44:40 +0800

    Update Configuration.cs

--- Wenote.Core/Configuration.cs
+++ Wenote.Core/Configuration.cs
@@ -2,8 +2,8 @@
 
 namespace Wenote.Core {
     public class Configuration {
-        public static string ServerUrl => "http://8.131.62.225:443/";
-        public static string SiteUrl => "http://8.131.62.225/";
+        public static string ServerUrl => "https://api.wenote.site/";
+        public static string SiteUrl => "https://wenote.site/";
         public static string DefaultAvatarUrl => "/uploads/3ea6beec64369c2642b92c6726f1epng_0c0d93eded.png";
     }
 }
